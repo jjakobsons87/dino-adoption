37d8cc5110d25a245e44cdeffc886ac878e839b0
Ted J <tedmjablonowski@gmail.com>
2022-07-05 20:43:00 -0400

    All 10 dino images imported

---
+++ client/src/pages/DinoHome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import TRexImage from "../assets/images/trex.jpg"
+import SpinoImage from "../assets/images/spinosaurus.png"
+import RaptorImage from "../assets/images/raptor.jpg"
+import DilophImage from "../assets/images/dilophosaurus.png"
+import AlloImage from "../assets/images/allosaurus.jpg"
+import AnkylImage from "../assets/images/ankylosaurus.jpg"
+import BrontoImage from "../assets/images/brontosaurus.jpg"
+import ParaImage from "../assets/images/parasaurilophus.jpg"
+import StegaImage from "../assets/images/stegasaurus.png"
