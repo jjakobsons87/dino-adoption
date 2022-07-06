7b0b25a72b260816ccbac795f4290dab5fa33d9f
Ted J <tedmjablonowski@gmail.com>
2022-07-06 07:46:30 -0400

    Image locations fixed because I be a toolbag

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,9 @@
-import {
-  ApolloProvider,
-  ApolloClient,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
-import logo from "./logo.svg";
-import "./App.css";
+import AdoptionPage from "./pages/adoption";
 
-const httpLink = createHttpLink({
-  uri: "/graphql",
-});
-
-const client = new ApolloClient({
-  link: httpLink,
-  cache: new InMemoryCache(),
-});
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </ApolloProvider>
+   <AdoptionPage></AdoptionPage>
   );
 }
 
