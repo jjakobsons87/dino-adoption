c6d1c0316d5ebebf8d3fa09d46965855a7cb8408
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-07 17:08:03 -0400

    adduser and login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -72,7 +72,30 @@ const resolvers = {
             .populate('description')
             .populate('inventory')
         }
-    
+    },
+
+    Mutation: { 
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
     }
 };
 
