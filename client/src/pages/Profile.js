084599f8edc5908e52dfa5931b4eca48d41a8551
Kyle Richnafsky <7krich@gmail.com>
2022-07-07 11:04:10 -0400

    added base files for profile pg components

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
+import Auth from '../utils/auth';
+
+//components
+import Footer from '../components/Footer';
+
+const Profile = (props) => {
+    return (
+        <Footer></Footer>
+    )
+};
+
+export default Profile;
