1dfc1043b36384bdf8675c090bae0088ca3b3e28
Kyle Richnafsky <7krich@gmail.com>
2022-07-07 11:17:21 -0400

    added user profile image

--- client/src/components/UserHero/index.js
+++ client/src/components/UserHero/index.js
@@ -1,1 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
+import User from '../../assets/images/rando.jpg'
+
+const UserHero = () => {
+    return (
+        <header className = "profile-header">
+
+            <div className="profile-container">
+                <img className="profile-pic"
+                    src={User}
+                    alt="User"
+                />
+            </div>
+        </header>
+    )
+}
