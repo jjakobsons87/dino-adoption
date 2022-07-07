5b2f013f5f2fd918ca3138b4526d06255ed76167
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-07 16:19:22 -0400

    start worok on resolvers.js

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,9 @@
+const { User, Comment, Accessory, Dino } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+
+};
+
+module.exports = resolvers;
