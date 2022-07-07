a62c64d51a368b5f9ba9aee3ff2c00bff622feeb
Kyle Richnafsky <7krich@gmail.com>
2022-07-07 12:53:53 -0400

    added placeholder for # of saved dinos & dinosOwned to User model

--- server/models/User.js
+++ server/models/User.js
@@ -20,6 +20,10 @@ const userSchema = new Schema(
       required: true,
       minlength: 5
     },
+    dinosOwned: {
+        type: Number,
+        required: true
+    },
     comments: [
       {
         type: Schema.Types.ObjectId,
