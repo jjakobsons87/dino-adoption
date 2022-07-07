ef8ceffe2d0016cebd927b6070822d781eb4060f
Rachel <rachel.gray7448@gmail.com>
2022-07-06 22:20:15 -0400

    possibly maybe got authentication for nav working

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,39 +1,42 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import { Container, Nav } from 'react-bootstrap';
+import Auth from "../utils/auth";
 
 export default function Nav({ currentPage, handlePageChange }) {
     return (
-        { isSignedIn ? (
-            <Navbar className="navbar" expand="lg">
-                <Container className="navbar-internal">
-                    <Navbar.Brand className="super-internal" href="#home">Home</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse className="super-internal" id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link className="super-internal" onClick={()=>handlePageChange("Profile")}>My Profile</Nav.Link>
-                            <Nav.Link className="super-internal" onClick={()=>handlePageChange("Adoption")}>Adoptable Dinosaurs</Nav.Link>
-                            <Nav.Link className="super-internal" onClick={()=>handlePageChange("Shop")}>Shop</Nav.Link>
-                            <Nav.Link className="super-internal" onClick={()=>handlePageChange("Logout")}>Logout</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        ) : (
-            <Navbar className="navbar" expand="lg">
-                <Container className="navbar-internal">
-                    <Navbar.Brand className="super-internal" href="#home">Home</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse className="super-internal" id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link className="super-internal" onClick={()=>handlePageChange("Adoption")}>Adoptable Dinosaurs</Nav.Link>
-                            <Nav.Link className="super-internal" onClick={()=>handlePageChange("Shop")}>Shop</Nav.Link>
-                            <Nav.Link className="super-internal" onClick={()=>handlePageChange("Login")}>Login</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        )}
+        <nav>
+            { Auth.loggedIn() ? (
+                <Navbar className="navbar" expand="lg">
+                    <Container className="navbar-internal">
+                        <Navbar.Brand className="super-internal" href="#home">Home</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse className="super-internal" id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link className="super-internal" onClick={()=>handlePageChange("Profile")}>My Profile</Nav.Link>
+                                <Nav.Link className="super-internal" onClick={()=>handlePageChange("Adoption")}>Adoptable Dinosaurs</Nav.Link>
+                                <Nav.Link className="super-internal" onClick={()=>handlePageChange("Shop")}>Shop</Nav.Link>
+                                <Nav.Link className="super-internal" onClick={()=>handlePageChange("Logout")}>Logout</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            ) : (
+                <Navbar className="navbar" expand="lg">
+                    <Container className="navbar-internal">
+                        <Navbar.Brand className="super-internal" href="#home">Home</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse className="super-internal" id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link className="super-internal" onClick={()=>handlePageChange("Adoption")}>Adoptable Dinosaurs</Nav.Link>
+                                <Nav.Link className="super-internal" onClick={()=>handlePageChange("Shop")}>Shop</Nav.Link>
+                                <Nav.Link className="super-internal" onClick={()=>handlePageChange("Login")}>Login</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            )}
+        </nav>
 
     )
 }
