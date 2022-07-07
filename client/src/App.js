faafe5df47259437d765d41ad594d8fbe70bda37
Ted J <tedmjablonowski@gmail.com>
2022-07-07 17:44:42 -0400

    Apollo Fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,24 @@
 import AdoptionPage from "./components/Adoption/index.js";
 import Footer from "./components/Footer/index.js"
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+} from "@apollo/client";
+import logo from "./logo.svg";
+import "./App.css";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
 
 function App() {
   return (
