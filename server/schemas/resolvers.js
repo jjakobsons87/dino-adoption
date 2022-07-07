386dee562bdb179d0e5e620c77e5f83cce27a169
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-07 16:32:29 -0400

    add me and get all dino resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,33 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('-__v -password')
+                    .populate('comments')
+                return userData;
+            }
+            throw new AuthenticationError('Not logged in');
+        },
 
+        // get all dinos 
+        dinos: async () => {
+            return Dino.find()
+            .populate('bio')
+            .populate('species')
+            .populate('diet')
+            .populate('gender')
+            .populate('aggressiveness')
+            .populate('humanCasualties')
+            .populate('fenceRequirement')
+            .populate('name')
+            .populate('savedCount')
+            .populate('age')
+            .populate('inventory')
+        }
+    }
 };
 
 module.exports = resolvers;
