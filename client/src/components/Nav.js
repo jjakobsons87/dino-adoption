748ac931c3cab3448bd1ac39b7df935bc71e75d6
Rachel <rachel.gray7448@gmail.com>
2022-07-06 21:17:52 -0400

    added basic Nav component

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import Navbar from 'react-bootstrap';
-
+import Navbar from 'react-bootstrap/Navbar';
+import { Container, NavDropdown, Nav } from 'react-bootstrap';
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <Navbar className="navbar" expand="lg">
+            <Container className="navbar-internal">
+                <Navbar.Brand className="super-internal" href="#home">Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse className="super-internal" id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link className="super-internal" onClick={()=>handlePageChange("Profile")}>My Profile</Nav.Link>
+                        <Nav.Link className="super-internal" onClick={()=>handlePageChange("Dinosaurs")}>Adoptable Dinosaurs</Nav.Link>
+                        <Nav.Link className="super-internal" onClick={()=>handlePageChange("Shop")}>Shop</Nav.Link>
+                        <Nav.Link className="super-internal" onClick={()=>handlePageChange("Logout")}>Logout</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
   )
 }
