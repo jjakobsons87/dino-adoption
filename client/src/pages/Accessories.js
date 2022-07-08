62c07e0425bf8a8159a1993c63e9f4ee6e3e7743
Ted J <tedmjablonowski@gmail.com>
2022-07-07 20:36:29 -0400

    Pushing Kyle's development put to my branch with with accessories route with the page itself started

---
+++ client/src/pages/Accessories.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import AccessoriesPage from "../components/Accessories/index";
+
+const Accessories = () => {
+    return (
+        <div>
+            <main>
+                <AccessoriesPage></AccessoriesPage>
+            </main>
+        </div>
+    )
+};
+
+export default Accessories;
