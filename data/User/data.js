e2c6ff02826ad201fa41b08cbd77619ee211b305
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-07 20:55:22 -0400

    add mock data

---
+++ data/User/data.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    username: "test1",
+    email: "test1@test.com",
+    password: "test12345",
+    dinosOwned: 3,
+  },
+  {
+    username: "test2",
+    email: "test2@test.com",
+    password: "test12345",
+    dinosOwned: 1,
+  },
+  {
+    username: "test3",
+    email: "test3@test.com",
+    password: "test12345",
+    dinosOwned: 0,
+  },
+  {
+    username: "test4",
+    email: "test4@test.com",
+    password: "test12345",
+    dinosOwned: 5,
+  },
+  {
+    username: "test5",
+    email: "test5@test.com",
+    password: "test12345",
+    dinosOwned: 1,
+  },
+  {
+    username: "test6",
+    email: "test6@test.com",
+    password: "test12345",
+    dinosOwned: 2,
+  },
+  {
+    username: "test7",
+    email: "test7@test.com",
+    password: "test12345",
+    dinosOwned: 4,
+  },
+  {
+    username: "test8",
+    email: "test8@test.com",
+    password: "test12345",
+    dinosOwned: 6,
+  },
+  {
+    username: "test9",
+    email: "test9@test.com",
+    password: "test12345",
+    dinosOwned: 3,
+  },
+];
