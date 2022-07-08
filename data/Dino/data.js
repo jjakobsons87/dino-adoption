3da6afd84a449986e6fe72ba4b1d16374aa1f0c9
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-08 07:36:01 -0400

    update data

---
+++ data/Dino/data.js
@@ -0,0 +1,18 @@
+import dateFormat from ("/server/utils/dateFormat");
+
+module.exports [
+    {
+        bio: "",
+        species: "",
+        diet: "",
+        gender: "",
+        aggressiveness: 6,
+        humanCasualties: 2,
+        fenceRequirement: 1,
+        name: "",
+        age: 12,
+        inventory: 1,
+        addedAt: Date.now
+
+    }
+]
