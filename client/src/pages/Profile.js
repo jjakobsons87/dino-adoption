03ab19642290d91aaf43cc424fd460199410ee6b
Kyle Richnafsky <7krich@gmail.com>
2022-07-07 20:09:26 -0400

    restructured files & set up react router

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
-import Auth from '../utils/auth';
+
 
 //components
 import LikedDinos from '../components/LikedDinos';
