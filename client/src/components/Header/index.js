d7a86bc72011244370a3e8a12d3b9da607e43dc8
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-07-08 23:40:35 -0400

    Uncommented to find broken component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,36 +1,64 @@
 import React, { useState } from "react";
-import Nav from "../Nav/Nav";
-import Adoption from "../Adoption";
-import Profile from "./Profile";
-import Shop from "./Shop";
-import Login from "./Login";
+// import Nav from "../Nav/Nav";
+// import Adoption from "../Adoption";
+// import Profile from "./Profile";
+// import Shop from "./Shop";
+// import Login from "./Login";
 
 export default function Header() {
     const [currentPage, setCurrentPage] = useState("Adoption");
 
     const renderPage = () => {
-        if (currentPage === "Adoption") {
-            return <Adoption />;
-        }
-        if (currentPage === "Profile") {
-            return <Profile />;
-        }
-        if (currentPage === "Shop") {
-            return <Shop />;
-        }
-        if (currentPage === "Login") {
-            return <Login />;
-        }
+        // if (currentPage === "Adoption") {
+        //     return <Adoption />;
+        // }
+        // if (currentPage === "Profile") {
+        //     return <Profile />;
+        // }
+        // if (currentPage === "Shop") {
+        //     return <Shop />;
+        // }
+        // if (currentPage === "Login") {
+        //     return <Login />;
+        // }
     };
 
     const handlePageChange = (page) => setCurrentPage(page);
     return (
-        <div>
-            <Nav
+        <header>
+            {/* <Nav
                 currentPage={currentPage}
                 handlePageChange={handlePageChange}
             />
             {renderPage()}
+            <div
+       id="intro"
+       class="bg-image"
+       style="
+              background-image: url(https://mdbcdn.b-cdn.net/img/new/fluid/city/113.jpg);
+              height: 100vh;
+              "
+       >
+    <div class="mask text-white" style="background-color: rgba(0, 0, 0, 0.8)">
+      <div class="container d-flex align-items-center text-center h-100">
+        <div>
+          <h1 class="mb-5">This is title</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officiis molestiae
+            laboriosam numquam expedita ullam saepe ipsam, deserunt provident corporis,
+            sit non accusamus maxime, magni nulla quasi iste ipsa architecto? Autem!
+          </p>
         </div>
+      </div>
+    </div>
+  </div> */}
+  <p>Show something here</p>
+        </header>
     );
 }
+
+//-- To make Navbar disappear - code needed for Nav before css
+//   <nav class="navbar navbar-expand-lg fixed-top navbar-scroll">
+//     <div class="container-fluid">
+//     </div>
+//   </nav>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import AdoptionPage from "../components/Adoption/index";
+import Header from '../components/Header/index';
 
 const Home = () => {
     return (
         <div>
+            {/* <Header /> */}
             <main>
                 <AdoptionPage></AdoptionPage>
             </main>
