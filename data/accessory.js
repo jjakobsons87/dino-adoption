c81f5faa04c6919de771ee6186d9ff18c0df355d
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-09 10:10:41 -0400

    start adding data for cart

---
+++ data/accessory.js
@@ -0,0 +1,9 @@
+const products = [
+    {
+        name: "Trex Harness",
+        category: "Harness",
+        price: 250,
+        description: "This is a extra large harness for a Trex",
+        inventory: 15
+    }
+]
