80d66843c0b7c31c6d2e3e6c1f54512fa9e5d878
jjakobsons87 <98094445+jjakobsons87@users.noreply.github.com>
2022-07-09 10:16:16 -0400

    update data for accessorites

--- data/accessory.js
+++ data/accessory.js
@@ -3,7 +3,35 @@ const products = [
         name: "Trex Harness",
         category: "Harness",
         price: 250,
-        description: "This is a extra large harness for a Trex",
+        description: "This is an extra large harness for a Trex",
         inventory: 15
-    }
+    },
+    {
+        name: "XL Food Bowl",
+        category: "Feeding",
+        price: 75,
+        description: "This is a extra large feed bowl for a Trex",
+        inventory: 20
+    },
+    {
+        name: "Shock Collar",
+        category: "Protection",
+        price: 575,
+        description: "This is an extra large shock collar for a Trex. This is designed to be used with the larger fence protection system",
+        inventory: 25
+    },
+    {
+        name: "Fence Protection System - Med Dinos",
+        category: "Protection",
+        price: 25000,
+        description: "This fence protection system is designed to help keep your dino contained. This includes the wiring and fencing to keep them in a contained area.",
+        inventory: 10
+    },
+    {
+        name: "Trex feed",
+        category: "Feeding",
+        price: 500,
+        description: "Basic Trex feed grown in a lab",
+        inventory: 200
+    },
 ]
