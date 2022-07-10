6c139c73ac601eed283651ccd8edc495b9e8c8a2
Ted J <tedmjablonowski@gmail.com>
2022-07-10 08:52:09 -0400

    Font changes for all pages

--- client/src/components/Adoption/index.js
+++ client/src/components/Adoption/index.js
@@ -20,8 +20,8 @@ function AdoptionPage() {
 
              <div id="adoptDesc">
                 <div className="description">
-                    <h2 className="text-center text-white">The DDAA is here to fullfill all of your Dino-Related needs!</h2>
-                    <p className="text-center text-white">The DDAA, after much work and toil, has finally acquired the much sought after Dino-DNA, and, with the aquisition, we have also gained the ability to successfully clone our very own Dinosaurs! Who hasn't dreamed of riding a Triceratops to work, or having a Raptor rip your school bully to shreds? ...not that we recommend that, but you get the idea! Thanks to our own patented brand of genetic engineering you can own your very own dinosaur! But, be warned... this type of responsible will not come easy, and is not going to be cheap...
+                    <h2 className="text-center sectTitle">The DDAA is here to fullfill all of your Dino-Related needs!</h2>
+                    <p className="text-center text-white">As said before, the DDAA, after much work and toil, has finally acquired the much sought after Dino-DNA, and, with the aquisition, we have also gained the ability to successfully clone our very own Dinosaurs! Who hasn't dreamed of riding a Triceratops to work, or having a Raptor rip your school bully to shreds? ...not that we recommend that, but you get the idea! Thanks to our own patented brand of genetic engineering you can own your very own dinosaur! But, be warned... this type of responsible will not come easy, and is not going to be cheap...
                     <br></br><br></br>
                     There are two main categories of dinosaurs to choose from: Carnivores and Herbivores. Want to know more about a specific breed? Click on the images to visit their wikipedia page! Above all else, though... choose wisely!
                     </p>
--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -10,7 +10,7 @@ function Homepage() {
 
             <div id="siteDesc">
                 <div className="description">
-                    <h2 className="text-center text-white">The DDAA: True innovation</h2>
+                    <h2 className="text-center sectTitle">The DDAA: True innovation</h2>
                     <p className="text-center text-white">After many years of research, the Delusional Dinosaur Adoption Agency, or DDAA, has "acquired" genuine Dino DNA through very specific, non Government related channels. Thanks to the hard work of us and others. you too now have the distinct opportunity of owning your very own dinosaur! Fron the Tyrant King itself, the Tyrannasauus Rex, to the legendary long-neck, the Brachiosaurus, we have many options to choose from! 
                     <br></br><br></br>
                     Experience may vary, modifying DNA to control the size of your dinosaur at birth costs extra.
@@ -21,14 +21,14 @@ function Homepage() {
             <section>
 
             <div id="birthDiv" className="description">
-                    <h2 className="text-center text-white">Dinosaurs from birth</h2>
+                    <h2 className="text-center sectTitle">Adopt from birth to adult</h2>
                     <div className="homeDinos">
                         <img className="homeDinosPics"
                             src={RaptorBaby}
                             alt="Infant Raptor"
                         />
                     </div>
-                    <p className="text-center text-white">You have two adoptions in terms of the age that you will purchase: fully grown or the infant stage. Infant is recommended as it makes imprinting on the dinosaurs easier, thus they wil be more loyal to you. This has additional costs due to the care that comes with the adoption, but you will find it to be a much better route to go. 
+                    <p className="text-center text-white">You have two adoptions in terms of the age that you will purchase: fully grown or the infant stage. Infant is recommended as it makes imprinting on the dinosaurs easier, thus they wil be more loyal to you. This has additional costs due to the care that comes with the adoption, but you will find it to be a much better route to go. It is a far greater risk to adopt a full grown, set in it's ways Raptor, as opposed to an infant that you can develop a stronger bond with. Of course, adults have the advantage of protection, savagery, and striking fear in the hearts of your enemies. 
                     </p>
                 </div>
             </section>
