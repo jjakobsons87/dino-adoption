bf0da695edcda453df7065772c2de5aa306b73a8
Rachel <rachel.gray7448@gmail.com>
2022-07-10 19:07:16 -0400

    removed commented out array and imports from dinocard

--- client/src/components/Card/DinoCard.js
+++ client/src/components/Card/DinoCard.js
@@ -1,19 +1,6 @@
 import React, {useState} from "react";
 import SelectedDino from "../SelectedDino";
 import { useQuery } from "@apollo/client"
-// import TRexImage from "../../assets/images/trex.jpg";
-// import SpinoImage from "../../assets/images/spinosaurus.png";
-// import RaptorImage from "../../assets/images/raptor.jpg";
-// import DilophImage from "../../assets/images/dilophosaurus.png";
-// import AlloImage from "../../assets/images/allosaurus.jpg";
-// import AnkylImage from "../../assets/images/ankylosaurus.jpg";
-// import BrontoImage from "../../assets/images/brontosaurus.jpg";
-// import ParaImage from "../../assets/images/parasaurilophus.jpg";
-// import StegaImage from "../../assets/images/stegasaurus.jpg";
-// import TriImage from "../../assets/images/Triceratops.jpg";
-// import PteroImage from "../../assets/images/pterandon.jpg";
-// import PachyImage from "../../assets/images/pachy.jpg";
-// import { Link } from 'react-router-dom';
 import { QUERY_DINOS, QUERY_DINO } from "../../../src/utils/queries";
 
 import {
@@ -29,57 +16,6 @@ import {
 export default function DinoCard() {
 
     const [currentDino, setCurrentDino] = useState();
-
-    // const [dinos] = useState([
-    //     {
-    //         name: "Tyrannasaurus Rex",
-    //         source: TRexImage,
-    //     },
-    //     {
-    //         name: "Spinosaurus",
-    //         source: SpinoImage,
-    //     },
-    //     {
-    //         name: "Utahraptor",
-    //         source: RaptorImage,
-    //     },
-    //     {
-    //         name: "Dilophosaurus",
-    //         source: DilophImage,
-    //     },
-    //     {
-    //         name: "Allosaurus",
-    //         source: AlloImage,
-    //     },
-    //     {
-    //         name: "Ankylosaurus",
-    //         source: AnkylImage,
-    //     },
-    //     {
-    //         name: "Brachiosaurus",
-    //         source: BrontoImage,
-    //     },
-    //     {
-    //         name: "Parasaurilophus",
-    //         source: ParaImage,
-    //     },
-    //     {
-    //         name: "Stegasaurus",
-    //         source: StegaImage,
-    //     },
-    //     {
-    //         name: "Triceratops",
-    //         source: TriImage,
-    //     },
-    //     {
-    //         name: "Pterandon",
-    //         source: PteroImage,
-    //     },
-    //     {
-    //         name: "Pachycepalosaurus",
-    //         source: PachyImage,
-    //     },
-    // ]);
 
     const { loading, error, data } = useQuery(QUERY_DINOS);
 
