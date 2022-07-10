c738df75d7b719d5a451b93bae587b5e542111da
Ted J <tedmjablonowski@gmail.com>
2022-07-10 09:20:22 -0400

    Font adjusted according to screen size

--- client/src/components/Homepage/index.js
+++ client/src/components/Homepage/index.js
@@ -26,19 +26,19 @@ function Homepage() {
                     <h2 className="text-center sectTitle">The Challenges</h2>
                     <p className="text-center text-white">We never said that owning a dinosaur would be easy. These animals require A LOT of space to roam around, they need to remain active or these is a "small" chance that they will become aggressive. Plenty of stimulation will be required! Fortunately, we do offer accessories, food and toys to make the transition easier for both you and the dino.
                     <br></br><br></br>
-                    This will not be like owning a dog, cat or even more exotic animals from our time. Are current lineup is far bigger, meaner and far more dangerous than anything you can imaging; but don't let that deter you! They can be loving and protective under the right circumstances, and believe us... no one will think twice of invading your home or doing you and your family harm, lest they get blinded and mauled by your pet Dilophosaurus.
+                    This will not be like owning a dog, cat or even more exotic animals from our time. Our current lineup is much bigger, meaner and far more dangerous than anything you can imagine; but don't let that deter you! They can be loving and protective under the right circumstances, and believe us... no one will think twice of invading your home or doing you and your family harm, lest they get blinded and mauled by your pet Dilophosaurus.
                     </p>
             </div>
 
             <div id="birthDiv" className="description">
-                    <h2 className="text-center sectTitle">Adopt from birth to adult</h2>
+                    <h2 className="text-center sectTitle">Infant Stage Adoption</h2>
                     <div className="homeDinos">
                         <img className="homeDinosPics"
                             src={RaptorBaby}
                             alt="Infant Raptor"
                         />
                     </div>
-                    <p className="text-center text-white">You have two adoptions in terms of the age that you will purchase: fully grown or the infant stage. Infant is recommended as it makes imprinting on the dinosaurs easier, thus they wil be more loyal to you. This has additional costs due to the care that comes with the adoption, but you will find it to be a much better route to go. It is a far greater risk to adopt a full grown, set in it's ways Raptor, as opposed to an infant that you can develop a stronger bond with. Of course, adults have the advantage of protection, savagery, and striking fear in the hearts of your enemies. 
+                    <p className="text-center text-white">Your dinosaurs will be sent to you as infants to make both delivery and the transitions easier. Infant is highly recommended as it makes imprinting on the dinosaurs easier, thus they wil be more loyal to you. This has additional costs due to the care that comes with the adoption, but you will find it to be a much better route to go. It is a far greater risk to adopt a full grown, set in it's ways Raptor, as opposed to an infant that you can develop a stronger bond with. Of course, adults have the advantage of protection, savagery, and striking fear in the hearts of your enemies. 
                     </p>
                 </div>
             </section>
