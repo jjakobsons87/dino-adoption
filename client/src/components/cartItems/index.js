242a04e4e7d5c3a8b3cf8930c7692bd1b271e904
Kyle Richnafsky <7krich@gmail.com>
2022-07-11 15:13:46 -0400

    added structure for cart

---
+++ client/src/components/cartItems/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const cartItems = () => {
+
+    return (
+        <div className="cart-item-card">
+            {/* placeholder to import selected dino from queries */}
+            {/* placeholder to import selected dino name from queries */}
+            <div className="quantity">Quantity:
+            </div>
+            <div className="total">Cost:
+            </div>
+        </div>
+    )
+};
+
+export default cartItems;
--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 
-
 //components
+import CartItems from '../components/cartItems';
 
 const Cart = (props) => {
     return (
         <div className="cart-container">
+            <CartItems/>
         </div>
     )
 };
