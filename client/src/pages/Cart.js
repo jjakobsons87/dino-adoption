3e5d7df7873b22e1410fc9699322494e35cdc22d
Kyle Richnafsky <7krich@gmail.com>
2022-07-11 16:11:43 -0400

    added quantity logic for cart

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -8,6 +8,18 @@ import CartHero from '../components/CartHero';
 const Cart = (props) => {
 
     const [cartItems, setCartItems] = useState([]);
+    const onAdd = (dino) => {
+        const exist = cartItems.find(x=> x.id === dino.id);
+        if(exist) {
+            setCartItems(
+                cartItems.map((x) =>
+                    x.id === dino.id ? {...exist, qty: exist.qty + 1} : x
+                )
+            );
+        } else {
+            setCartItems([...cartItems, {...dino, qty: 1 }]);
+        }
+    };
 
     return (
         <div className="cart-container">
