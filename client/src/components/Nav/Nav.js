9087837b4ca194a2d5812491d4b028c4e2f5953d
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-07-11 01:27:51 -0400

    Adjusting Nav bar to attempt solid when scroll

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,6 +4,7 @@ import { Container, Nav } from 'react-bootstrap';
 import Auth from "../../utils/auth";
 
 export default function Navigation({ currentPage, handlePageChange }) {
+    
     return (
         <nav>
             { Auth.loggedIn() ? (
@@ -39,4 +40,16 @@ export default function Navigation({ currentPage, handlePageChange }) {
         </nav>
 
     )
+}
+
+window.onscroll = function() {scrollFunction()};
+ 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    
+    document.getElementById("navbar").style.background = "#501e27";
+  } else {
+   
+    document.getElementById("navbar").style.background = "none";
+  }
 }
