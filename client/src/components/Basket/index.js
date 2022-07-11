19fdb163433b4d5f463411e75abd51cb0bb0d0a3
Kyle Richnafsky <7krich@gmail.com>
2022-07-11 18:21:42 -0400

    defined prices and added price if item is in cart

--- client/src/components/Basket/index.js
+++ client/src/components/Basket/index.js
@@ -4,6 +4,10 @@ import React from 'react';
 const Basket = (props) => {
 
 const {cartItems, onAdd, onRemove } = props;
+const itemsPrice = cartItems.reduce((a, c) => a + c.price * c.qty, 0);
+const taxPrice = itemsPrice * .14;
+const shippingPrice = itemsPrice > 5000 ? 0 : 1000;
+const totalPrice = itemsPrice + taxPrice + shippingPrice;
 
     return (
         <div className="basket-container">
@@ -29,6 +33,15 @@ const {cartItems, onAdd, onRemove } = props;
                     </div>
                 </div>
             ))}
+            {cartItems.length !== 0 && (
+                <>
+                <hr></hr>
+                <div className="price">
+                    <div>Items Price</div>
+                    <div> ${itemsPrice.toFixed(2)}</div>
+                </div>
+                </>
+            )}
         </div>
     )
 };
