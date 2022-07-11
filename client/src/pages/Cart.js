b172805c7368570700556e2940c6dd8207963026
Kyle Richnafsky <7krich@gmail.com>
2022-07-11 11:08:34 -0400

    removed placeholders in cart pg

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -3,16 +3,10 @@ import { Navigate, useParams } from 'react-router-dom';
 
 
 //components
-import LikedDinos from '../components/LikedDinos';
-import SelectedDino from '../components/SelectedDino';
-import UserHero from '../components/UserHero';
 
 const Cart = (props) => {
     return (
-        <div className="profile-container">
-            <UserHero></UserHero>
-            <LikedDinos></LikedDinos>
-            <SelectedDino></SelectedDino>
+        <div className="cart-container">
         </div>
     )
 };
