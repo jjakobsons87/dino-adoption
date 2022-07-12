74a9cf1f98de9438ef70012387ff70b628cee996
a-wiles <98373402+a-wiles@users.noreply.github.com>
2022-07-12 00:57:49 -0400

    Added login form to modal

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,12 +1,50 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { useMutation } from '@apollo/client';
+import { LOGIN_USER } from '../../utils/mutations';
+import Form from 'react-bootstrap/Form';
 
-function Example() {
+import Auth from '../../utils/auth';
+
+export default function Login (props) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+      event.preventDefault();
+  
+      try {
+        const { data } = await login({
+          variables: { ...formState },
+        });
+  
+        Auth.login(data.login.token);
+      } catch (e) {
+        console.error(e);
+      }
+  
+      // clear form values
+      setFormState({
+        email: '',
+        password: '',
+      });
+    };
 
   return (
     <>
@@ -21,21 +59,47 @@ function Example() {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
+          <Modal.Title>Login</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          I will not close if you click outside me. Don't even try to press
-          escape key.
-        </Modal.Body>
-        <Modal.Footer>
+        <Form onSubmit={handleFormSubmit}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                className="form-input"
+                name="email"
+                id="email"
+                type="email"
+                placeholder="name@example.com"
+                value={formState.email}
+                onChange={handleChange}
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlInput2"
+            >
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                className="form-input"
+                name="password"
+                id="password"
+                type="password"
+                placeholder="******"
+                value={formState.password}
+                onChange={handleChange}
+                autoFocus
+                />
+            </Form.Group>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
+          <Button className="btn d-block w-100" type="submit">
+                Submit</Button>
+                </Form>
+                {error && <div>Login failed</div>}
       </Modal>
     </>
   );
 }
 
-export default Example;
