f43ba7ffe6829c9667dd659745b056a238ecbee7
Kyle Richnafsky <7krich@gmail.com>
2022-07-12 15:08:36 -0400

    working to bring liked dino's to profile pg

--- client/src/components/LikedDinos/index.js
+++ client/src/components/LikedDinos/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import DinoCard from '../Card/DinoCard';
-
-const LikedDinos = () => {
+const LikedDinos = ({ dinos }) => {
     return (
         <div className = "liked-container">
-            <DinoCard></DinoCard>
+            <div className="liked-header">
+                <span className="liked-text">Liked Dinos</span>
+            </div>
+            {dinos &&
+            dinos.map(dino => (
+                <div className="liked-dino" key={dino._id}>
+                    {dino.name}
+                    <Link to={`/adoption/${dino.name}`}>
+                    {dino.name}
+              </Link>
+                </div>
+            ))}
         </div>
     )
 };
