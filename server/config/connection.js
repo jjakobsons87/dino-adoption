f0c055ff6efb392e8c5a63a0b2efa8cddd510e43
Jeremy Jakobsons <98094445+jjakobsons87@users.noreply.github.com>
2022-07-13 15:44:17 -0400

    Update connection.js
    this needs to be local host for everyone else - you can change it back to 0.0.0.0 in your branch

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://0.0.0.0/deep-thoughts', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
