4b86f37facd07348e9aeed49e30c210dd3abbd5a
Kyle Richnafsky <7krich@gmail.com>
2022-07-13 18:09:04 -0400

    cart now functions

--- client/src/components/CartCard/index.js
+++ client/src/components/CartCard/index.js
@@ -18,19 +18,20 @@ const CartCard = (props) => {
     console.log(toCart)
 
     const onRemove = ( accessory ) => {
-        const exist = toCart.find((x) => x.id === accessory.id);
         // if 1 remove it from the list
-        if(exist.qty === 1) {
-            setToCart(toCart.filter((x) => x.id !== accessory.id))
+        if(accessory[1] - 1 === 0) {
+            setToCart(toCart.filter((x) => x[0]._id !== accessory[0]._id))
             // if greater than 1 decrease the quantity
         } else {
             setToCart(
                 toCart.map((x) =>
-                    x.id === accessory.id ? {...exist, qty: exist.qty - 1} : x
+                    x[0]._id === accessory[0]._id ? [{...x[0]}, x[1] - 1] : x
                 )
             );
+            setQuantity(prev => prev - 1);
         }
     };
+
     return (
         <div key={item._id} className="row">
         <div>
